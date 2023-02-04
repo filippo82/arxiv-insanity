80c0beef5486ba642f21feaafbf8d053e4284d24
Filippo B <filippo82@users.noreply.github.com>
2023-02-04 18:53:11 +0100

    Add docstrings to tasks and flow

--- pipelines/flows/basic_flow.py
+++
@@ -1,9 +0,0 @@
-from prefect import flow, get_run_logger
-
-@flow(name="Testing")
-def basic_flow():
-    logger = get_run_logger()
-    logger.warning("The fun is about to begin")
-
-if __name__ == "__main__":
-    basic_flow()
--- pipelines/flows/download_dataset_from_kaggle.py
+++ pipelines/flows/download_dataset_from_kaggle.py
@@ -11,30 +11,39 @@ from prefect import task
 from prefect.task_runners import SequentialTaskRunner
 from prefect_gcp.cloud_storage import GcsBucket
 
-# from prefect_gcp import GcpCredentials, GcsBucket
-# from prefect_gcp.credentials import GcpCredentials
+FN = "arxiv-metadata-oai-snapshot.json"
+FN_PROCESSED = "arxiv-metadata-oai-snapshot-processed.jsonl"
 
-fn = "arxiv-metadata-oai-snapshot.json"
-fn_processed = "arxiv-metadata-oai-snapshot-processed.jsonl"
+PREFECT_STORAGE_BLOCK_GCS_BUCKET = "block-bucket-arxiv-data"
 
 
 @task
 def download_dataset():
+    """Download the metadata of all arXiv preprints from Kaggle.
+    The [arXiv Dataset](https://www.kaggle.com/datasets/Cornell-University/arxiv)
+    is updated on a monthly basis.
+    """
     logger = get_run_logger()
+
+    # Create Kaggle client
     api = KaggleApi()
     api.authenticate()
 
-    logger.info("Start - Download dataset")
+    logger.info("Start downloading dataset")
     api.dataset_download_files("Cornell-University/arxiv", quiet=False, unzip=True)
-    logger.info("End - Download dataset")
+    logger.info("Dataset has been downloaded")
 
 
 @task
-def prepare_json_for_bigquery():
+def prepare_jsonl_for_bigquery():
+    """Process the downloaded metadata file so that it can be directly loaded
+    into BigQuery. This [comment](https://www.kaggle.com/datasets/Cornell-University/arxiv/discussion/376149)
+    explains why this additional processing is required.
+    """
     logger = get_run_logger()
 
     # BigQuery field names cannot contain `-`.
-    # Hence, we replace `-` with `_`
+    # Hence, we replace `-` with `_`.
     invalid_field_names = {
         "journal-ref": "journal_ref",
         "report-no": "report_no",
@@ -46,7 +55,8 @@ def prepare_json_for_bigquery():
 
     log_step = 100000
 
-    with open(fn) as f, open(fn_processed, "w") as fp:
+    logger.info("Start processing JSONL file")
+    with open(FN) as f, open(FN_PROCESSED, "w") as fp:
         cnt = 0
         for line in f:
             article = json.loads(line)
@@ -69,6 +79,7 @@ def prepare_json_for_bigquery():
                     processed[k] = v
 
             # Validate processed entry
+            # TODO
 
             json.dump(processed, fp)
             fp.write("\n")
@@ -81,29 +92,50 @@ def prepare_json_for_bigquery():
 
 
 @task
-def copy_processed_file_to_bucket():
-    gcp_cloud_storage_bucket_block = GcsBucket.load("block-bucket-arxiv-data")
-    path = gcp_cloud_storage_bucket_block.upload_from_path(fn_processed, f"kaggle/{fn_processed}")
+def copy_processed_file_to_bucket() -> str:
+    """Copy the processed file to a GCS bucket.
+
+    Returns
+    -------
+    str
+        URI of the processed file in the GCS bucket.
+    """
+    gcp_cloud_storage_bucket_block = GcsBucket.load(PREFECT_STORAGE_BLOCK_GCS_BUCKET)
+    path = gcp_cloud_storage_bucket_block.upload_from_path(FN_PROCESSED, f"kaggle/{FN_PROCESSED}")
     return path
 
 
 @task
-def log_task(name):
+def log_prefect_version(name: str):
+    """Simple logger which prints the Prefect version.
+
+    Parameters
+    ----------
+    name : str
+        The name of a dude or dudette.
+    """
     logger = get_run_logger()
     logger.info("Hello %s!", name)
     logger.info("Prefect Version = %s 🚀", prefect.__version__)
 
 
 @flow(task_runner=SequentialTaskRunner())
-def log_flow(name: str = "Jake"):
+def flow_get_arxiv_kaggle_dataset(name: str = "Filippo"):
+    """Download from Kaggle, process, and copy the JSONL file to a GCS bucket.
+
+    Parameters
+    ----------
+    name : str, optional
+        The name of a dude or dudette, by default "Filippo".
+    """
     logger = get_run_logger()
-    log_task(name)
+    log_prefect_version(name)
     download_dataset()
-    prepare_json_for_bigquery()
+    prepare_jsonl_for_bigquery()
     path = copy_processed_file_to_bucket()
-    logger.info(f"The procesed file has been stored at {path}")
+    logger.info(f"The processed file has been stored at {path}")
 
 
 if __name__ == "__main__":
     name = sys.argv[1]
-    log_flow(name)
+    flow_get_arxiv_kaggle_dataset(name)
